d78964bb5063a31010808f0d938b58f2b6b750d2
Lucas Edson <33698741+lucasedson@users.noreply.github.com>
2024-04-30 09:54:59 +0000

    yup

--- main.py
+++ main.py
@@ -9,14 +9,16 @@ from pytube import YouTube
 
 formater = SRTFormatter()
 
-url = "https://www.youtube.com/watch?v=RejZhH-Dfjs&ab_channel=FlowPodcast"
+# url = "https://www.youtube.com/watch?v=JFZJM7D9KCU&ab_channel=RicardoNunes"
+url = input("Digite a URL do video: ")
 titulo_video = YouTube(url).title
 # url = url.replace("-", "")
-video_id = re.search(r"v=(\w+-\w+)", url).group(1)
-# video_id = video_id.replace("&", "")
+video_id = re.search(r"v=(.+&)", url).group(1)
+video_id = video_id.replace("&", "")
 print(video_id)
 
 srt = YouTubeTranscriptApi.get_transcript(video_id, languages=['pt'])
+
 
 srt = formater.format_transcript(srt)
 
@@ -34,12 +36,10 @@ for i in range(0, tamanho_srt//fator):
     # time.sleep(10)
 
     legenda = str(srt[i*fator:(i+1)*fator])
-    print(type(legenda))
 
 
-
-    mensagem = "Abaixo está um trecho de legenda de uma entrevista no Podcast. Eu não quero a transcrição. Mas preciso que no script me dê o tema abordado nesse range e os seus respectivos minutos. A saída tem que ter rigorosamente o seguinte formato: [MINUTOS: HH:mm:ss - HH:mm:ss] - [TEMA]:[TITULO]" + legenda
-    
+    # mensagem = "Abaixo está um trecho de legenda de uma entrevista no Podcast. Eu não quero a transcrição. Mas preciso que no script me dê o tema abordado nesse range e os seus respectivos minutos. A saída tem que ter rigorosamente o seguinte formato: [MINUTOS: HH:mm:ss - HH:mm:ss] - [TEMA]:[TITULO]" + legenda
+    mensagem = "Abaixo está a legenda de um filme. Eu não quero a transcrição. Mas preciso que no script me resuma o que passa nesse range do filme e os seus respectivos minutos. A saída tem que ter rigorosamente o seguinte formato: [MINUTOS: HH:mm:ss - HH:mm:ss] - [TITULO]:[RESUMO]" + legenda
     response = client.chat.completions.create(
         model="gpt-4",
         messages=[{"role": "user", "content": mensagem}],)
@@ -48,5 +48,5 @@ for i in range(0, tamanho_srt//fator):
     texto_final += response.choices[0].message.content + "\n\n"
 
 
-with open(titulo_video+"resumo.txt", "w") as f:
+with open("AI_RESUMO"+titulo_video+".txt", "w") as f:
     f.write(texto_final)
